fd92ed089eba332f0382268a62cad427ebc0540a
HappyMan <HappyMan@HappyCom>
2016-09-11 16:48:21 +0900

    hotfix branch change a code

--- HelloWorld/src/Helloworld/Helloworld.java
+++ HelloWorld/src/Helloworld/Helloworld.java
@@ -4,6 +4,7 @@ public class Helloworld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		System.out.println("소프트웨어학과");
 		// TODO Auto-generated method stub
 
 	}
