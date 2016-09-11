17b0f5beeee6319432e93dffbe2b32ad4b4f8084
HappyMan <HappyMan@HappyCom>
2016-09-11 16:42:21 +0900

    added print line

--- HelloWorld/src/Helloworld/Helloworld.java
+++ HelloWorld/src/Helloworld/Helloworld.java
@@ -4,6 +4,7 @@ public class Helloworld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		System.out.println("2013041054 YeonJunHeum");
 		// TODO Auto-generated method stub
 
 	}
